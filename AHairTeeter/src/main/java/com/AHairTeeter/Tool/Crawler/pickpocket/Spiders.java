b35e3127562c1e0bc6e0313da056fb46ecd9a4db
Z-DEREN <zhangderen233@gmail.com>
2019-10-08 17:28:54 +0800

    爬虫相关1

--- AHairTeeter/src/main/java/com/AHairTeeter/Tool/Crawler/pickpocket/Spiders.java
+++ AHairTeeter/src/main/java/com/AHairTeeter/Tool/Crawler/pickpocket/Spiders.java
@@ -7,59 +7,62 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 /**
+ * 多样爬虫工具
  * 
  * @author 好人
  *
  */
 public class Spiders {
 
-	
+	/**
+	 * 
+	 * 
+	 * 1.静态爬虫 1.1.爬取接口 1.1.1.高匿动态用户 1.1.2.高匿 1.1.3.动态用户 1.1.999.裸奔 1.2.爬取页面
+	 * 1.2.1.高匿动态用户 1.2.2.高匿 1.2.3.动态用户 1.2.999.裸奔 
+	 * 
+	 * 
+	 * 2.动态爬虫 2.1.爬取动态页面 1.2.1.高匿用户 1.2.999.裸奔
+	 * 
+	 * WebDrivers 模拟浏览器动态爬取
+	 * 
+	 * 
+	 * @param url
+	 * @param num
+	 * @return
+	 * @throws InterruptedException
+	 */
 	public String spiders(String url, int num) throws InterruptedException {
-		TypeTest user = new TypeTest();
-//		List<Object> list = user.Seluser();
-//		Random random = new Random();
-//		int n = random.nextInt(list.size());
-//		Map<String, String> passmap = (Map<String, String>) list.get(n);
-//		System.out.println(passmap.get("useragent"));
 		Document doc = null;
 		Connection.Response res = null;
 		String Sdoc = null;
-		try {
-			switch (num) {
-			case 1:
-				doc = Jsoup.connect(url).header("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) " + "AppleWebKit/537.36 (KHTML, like Gecko) "
-						+ "Chrome/47.0.2526.106 BIDUBrowser/8.7 Safari/537.36").ignoreContentType(true).get();
-				Sdoc = doc.html();
-				break;
-			case 2:
-//				doc = Jsoup.connect(url).header("user-agent", passmap.get("useragent")).get();
-				Sdoc = doc.html();
-				break;
-			case 3:
-//				res = Jsoup.connect(url).header("user-agent", passmap.get("useragent")).method(Method.POST).execute();
-				res.toString();
-				break;
-			case 4:
-				WebDrivers wd = new WebDrivers();
-				Sdoc = wd.Dynamic(url);
-				break;
-			case 5:
-				jsoupnew jsoupnew = new jsoupnew();
-				Sdoc = jsoupnew.webParseHtml(url);
-				break;
-			}
-			Sdoc = unicode.unicode(Sdoc);
+		switch (num) {
+		case 111:
 
-		} catch (IOException e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
+			break;
+		case 112:
+
+			break;
+		case 113:
+
+			break;
+		case 11999:
+
+			break;
+		case 121:
+
+			break;
+		case 122:
+
+			break;
+		case 123:
+
+			break;
+		case 12999:
+
+			break;
+			
 		}
 		return Sdoc;
-
 	}
-	
-	
-	
-	
-	
+
 }
